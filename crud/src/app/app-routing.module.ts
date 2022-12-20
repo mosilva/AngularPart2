3d278e55bc77ec09d746b148eaeb90f05fc2f7f2
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-20 18:36:02 -0300

    Create route

--- crud/src/app/app-routing.module.ts
+++ crud/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './users/not-found/not-found.component';
+import { CreateUserComponent } from './users/create-user/create-user.component';
+import { UsersComponent } from './users/users/users.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    component: UsersComponent,
+  },
+  {
+    path: 'create-users',
+    component: CreateUserComponent,
+  },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './users/users/users.component';
+import { CreateUserComponent } from './users/create-user/create-user.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NotFoundComponent } from './users/not-found/not-found.component';
+
+const MATERIAL = [MatToolbarModule, MatIconModule, MatButtonModule];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    CreateUserComponent,
+    NotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MATERIAL],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
