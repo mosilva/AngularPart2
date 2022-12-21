0e8e7a3ce8729ff817ddeb6fe074cc3e6023e975
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-21 12:40:54 -0300

    Create dynamic show users and install ngx-mask

--- crud/src/app/users/components/list/list.component.ts
+++ crud/src/app/users/components/list/list.component.ts
@@ -1,10 +1,11 @@
+import { User } from './../../models/user.model';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-
+  public users: User[] = JSON.parse(localStorage.getItem('USERS') || '[]');
 }
