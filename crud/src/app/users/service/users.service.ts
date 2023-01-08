25cd4a9cb37e60795696292999f5dde43c407d85
Marcelo Oliveira <marcelo.olisi@gmail.com>
2023-01-08 17:15:02 -0300

    update service

--- crud/src/app/users/service/users.service.ts
+++ crud/src/app/users/service/users.service.ts
@@ -1,3 +1,4 @@
+import { User } from './../models/user.model';
 import { State } from './../models/states.model';
 import { Injectable } from '@angular/core';
 
@@ -5,9 +6,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UsersService {
+  public users!: User[];
+
   constructor() {}
 
   public getStatesOfBrazil(): State[] {
     return [{ name: 'Acre', abbr: 'AC' }];
+  }
+
+  public saveUser(userNew: User): void {
+    this.users = JSON.parse(localStorage.getItem('USERS') || '[]');
+    this.users.push(userNew);
+    localStorage.setItem('USERS', JSON.stringify(this.users));
   }
 }
